78cfdea79ba693293bb706347f46255502b690b0
KirianCaumes <kirian.caumes@gmail.com>
2023-03-25 10:30:09 +0000

    Update robots.txt

--- next-sitemap.js
+++ next-sitemap.js
@@ -11,7 +11,7 @@ module.exports = {
         policies: [
             {
                 userAgent: '*',
-                allow: '/',
+                disallow: '/', // Disable SEO
             },
             {
                 userAgent: '*',
