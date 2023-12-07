4d8c2abfe9a359b5cbd156a4189d10c7c9330da0
KirianCaumes <kirian.caumes@gmail.com>
2023-12-07 19:06:30 +0100

    Upgrade to Node 20

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse, type NextRequest } from 'next/server'
+
+/**
+ * middleware
+ * @param req request
+ */
+// eslint-disable-next-line import/prefer-default-export
+export function middleware(req: NextRequest) {
+    if (req.nextUrl.pathname.startsWith('/image')) {
+        const url = req.nextUrl.clone()
+
+        if (process.env.NODE_ENV === 'development') {
+            url.href = `https://in-flames-closet.kiriancaumes.fr/${req.nextUrl.pathname}`
+            return NextResponse.redirect(url)
+        }
+
+        url.pathname = `/api${req.nextUrl.pathname}`
+        // Rewrite /image/... to /api/image/...
+        return NextResponse.rewrite(url)
+    }
+
+    return null
+}
